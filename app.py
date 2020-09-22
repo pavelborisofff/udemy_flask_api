831cacd1c5a8dfd6b4fd0b1fd4b13b53d20d93c6
Pavel Borisov <pavel.borisofff@gmail.com>
2020-09-22 18:29:17 +0300

    PosgreSQL for heroku

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
+import os
+
 from datetime import timedelta
 
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from flask_sqlalchemy import SQLAlchemy
 
 from resources.item import Item, ItemList
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.store import Store, StoreList
 
-from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
